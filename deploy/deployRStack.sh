e84c48196554fade25de25b3b2b77c5e671c3824
David Alexander <dalexander@pacificbiosciences.com>
2016-12-14 15:03:03 -0800

    Deploy python and R libs to new locations
    (Consolidated under /pbi/dept/consensus/bauhaus)
    
    Note that the lib argument to R install.packages was unnecessary since
    the R_LIBS env var becomes the first element of .libPath().

--- deploy/deployRStack.sh
+++ deploy/deployRStack.sh
@@ -12,15 +12,15 @@ module load gcc/4.8.2
 module load git/2.8.3
 
 # deployment
-rm -rf /mnt/secondary/Share/MLG/Bauhaus/R_libs
-mkdir -p /mnt/secondary/Share/MLG/Bauhaus/R_libs
 
-export R_LIBS=/mnt/secondary/Share/MLG/Bauhaus/R_libs
+export R_LIBS=/pbi/dept/consensus/bauhaus/rlibs
+rm -rf $R_LIBS
+mkdir -p $R_LIBS
 
 echo "$(env | grep R_LIBS)"
 echo "$(R -q -e 'print(.libPaths())')"
 
-R -q -e 'install.packages(c("Rcpp", "RcppEigen"), repos="http://cran.us.r-project.org", lib="/mnt/secondary/Share/MLG/Bauhaus/R_libs")'
-R -q -e 'install.packages(c("argparse", "argparser", "data.table", "devtools", "digest", "doMC", "doParallel", "dplyr", "feather", "ggplot2", "", "gridExtra", "gtools", "logging", "plyr", "tidyr", "wesanderson", "xml2"), repos="http://cran.us.r-project.org", lib="/mnt/secondary/Share/MLG/Bauhaus/R_libs")'
-R -q -e 'library(devtools) ; install_github("PacificBiosciences/pbbamr", lib="/mnt/secondary/Share/MLG/Bauhaus/R_libs")'
-R -q -e 'library(devtools) ; install_git("http://bitbucket.nanofluidics.com:7990/scm/sat/unitem-sat.git", lib="/mnt/secondary/Share/MLG/Bauhaus/R_libs")'
+R -q -e 'install.packages(c("Rcpp", "RcppEigen"), repos="http://cran.us.r-project.org")'
+R -q -e 'install.packages(c("argparse", "argparser", "data.table", "devtools", "digest", "doMC", "doParallel", "dplyr", "feather", "ggplot2", "", "gridExtra", "gtools", "logging", "plyr", "tidyr", "wesanderson", "xml2"), repos="http://cran.us.r-project.org")'
+R -q -e 'library(devtools) ; install_github("PacificBiosciences/pbbamr")'
+R -q -e 'library(devtools) ; install_git("http://bitbucket.nanofluidics.com:7990/scm/sat/unitem-sat.git")'
